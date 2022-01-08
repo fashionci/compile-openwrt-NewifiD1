4fa770af5193534c93222c8c5900e48cd86ed2a2
tpwonder <97324746+tpwonder@users.noreply.github.com>
2022-01-08 11:47:24 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,10 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+
+# Modify hostname
+sed -i 's/OpenWrt/Newifi D1/g' package/base-files/files/bin/config_generate
+
+# Modify the version number
+sed -i "s/OpenWrt /TPWonder build from Lede $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
